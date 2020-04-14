0492f3700bd55549a90b0f6e5b2cb4e9bd9aa5f5
Ricardo Huamani <ricardohuamanip@gmail.com>
2020-04-14 09:13:33 -0500

    move function def to top

--- week-1/day-1/ideal-gas.rb
+++ week-1/day-1/ideal-gas.rb
@@ -1,5 +1,9 @@
 R = 8.314
 
+def calculate_pressure(chemical_amount, temperature, volume)
+  return R * temperature * chemical_amount / volume
+end
+
 print "Chemical Amount: "
 chemical_amount = gets.chomp.to_f
 
@@ -8,9 +12,5 @@ temperature = gets.chomp.to_f
 
 print "Volume: "
 volume = gets.chomp.to_f
-
-def calculate_pressure(chemical_amount, temperature, volume)
-  return R * temperature * chemical_amount / volume
-end
 
 puts calculate_pressure(5, 500, 2) # Returns 10392.5
