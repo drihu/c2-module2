88b99b371f29ffa26ecd637ba29552ed6b2c4d44
Ricardo Huamani <ricardohuamanip@gmail.com>
2020-04-14 09:12:05 -0500

    remove return keyword

--- week-1/day-1/calculator.rb
+++ week-1/day-1/calculator.rb
@@ -1,26 +1,26 @@
 class Calculator
   def add(a, b)
-    return a + b
+    a + b
   end
 
   def subtract(a, b)
-    return a - b
+    a - b
   end
 
   def divide(a, b)
-    return a / b
+    a / b
   end
 
   def multiply(a, b)
-    return a * b
+    a * b
   end
 
   def exponentiate(a, b)
-    return a ** b
+    a ** b
   end
 
   def modulo(a, b)
-    return a % b
+    a % b
   end
 end
 
