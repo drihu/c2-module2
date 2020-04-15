9a7873a0d6cf6f852b883133f018d97181527c60
Ricardo Huamani <ricardohuamanip@gmail.com>
2020-04-15 18:13:42 -0500

    add each.rb

---
+++ week-1/day-3/each.rb
@@ -0,0 +1,11 @@
+def each(arr)
+  arr.each { |e| yield(e) }
+end
+
+test_array = [2, 3, 4, 5]
+each(test_array) { |element| puts "Number is #{element}" }
+# Has as a result:
+# Number is 2
+# Number is 3
+# Number is 4
+# Number is 5
