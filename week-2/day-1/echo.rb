6990617f2199578b980c57276eedb11d9188b47e
Ricardo Huamani <ricardohuamanip@gmail.com>
2020-04-20 11:38:36 -0500

    add echo.rb

---
+++ week-2/day-1/echo.rb
@@ -0,0 +1,21 @@
+def put_n(string, n)
+  if n.is_a?(Integer) && n > 0
+    n.times { puts string }
+  elsif !n.is_a? Integer
+    raise ArgumentError.new('Second argument should be an integer.')
+  elsif n <= 0
+    raise ArgumentError.new('Second argument should be greater than 0.')
+  end
+end
+
+def echo
+  puts 'What do you want to echo? '
+  string = gets.chomp
+  puts 'How many times do you want to repeat it? '
+  n = gets.chomp
+  n = (n.to_i == n.to_f) ? n.to_i : n.to_f
+
+  put_n(string, n)
+end
+
+echo
