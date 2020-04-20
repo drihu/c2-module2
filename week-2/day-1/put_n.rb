095684f49c460cad3f31acefbeed6d4e7326ad67
Ricardo Huamani <ricardohuamanip@gmail.com>
2020-04-20 11:05:29 -0500

    add put_n.rb

---
+++ week-2/day-1/put_n.rb
@@ -0,0 +1,11 @@
+def put_n(string, n)
+  n.times { puts string }
+end
+
+put_n("This will be printed 5 times!", 5)
+# Logs
+# This will be printed 5 times!
+# This will be printed 5 times!
+# This will be printed 5 times!
+# This will be printed 5 times!
+# This will be printed 5 times!
