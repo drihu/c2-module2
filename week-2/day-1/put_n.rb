f88c08d886e583c63d085c31a4844fc243af414e
Ricardo Huamani <ricardohuamanip@gmail.com>
2020-04-20 11:39:36 -0500

    update put_n.rb

--- week-2/day-1/put_n.rb
+++ week-2/day-1/put_n.rb
@@ -1,10 +1,11 @@
 def put_n(string, n)
-  if !n.is_a? Integer
+  if n.is_a?(Integer) && n > 0
+    n.times { puts string }
+  elsif !n.is_a? Integer
     raise ArgumentError.new('Second argument should be an integer.')
-  elsif n < 0
+  elsif n <= 0
     raise ArgumentError.new('Second argument should be greater than 0.')
   end
-  n.times { puts string }
 end
 
 put_n("This will be printed 5 times!", 5)
