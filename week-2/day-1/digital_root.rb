8ff8c15353fcf28be041c2b94cc824decfd3dadd
Ricardo Huamani <ricardohuamanip@gmail.com>
2020-04-21 00:29:46 -0500

    add comment

--- week-2/day-1/digital_root.rb
+++ week-2/day-1/digital_root.rb
@@ -11,6 +11,11 @@ def digital_root(number)
   end
 end
 
+# A digital root is the recursive sum of all the digits in a number.
+# Given n, take the sum of the digits of n. If that value has more
+# than one digit, continue reducing in this way until a single-digit
+# number is produced. This is only applicable to the natural numbers.
 p digital_root(16) # prints 7
 p digital_root(456) # prints 6
 p digital_root(132189) # prints 6
+p digital_root(493193) # prints 2
