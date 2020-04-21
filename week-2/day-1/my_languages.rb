958c193081a93821a56a1b27126c71986f9a1023
Ricardo Huamani <ricardohuamanip@gmail.com>
2020-04-21 00:04:31 -0500

    add comment

--- week-2/day-1/my_languages.rb
+++ week-2/day-1/my_languages.rb
@@ -2,6 +2,8 @@ def my_languages(results)
   results.sort_by { |l, s| -s }.map { |l, s| l if s >= 60 }.compact
 end
 
+# Return the list of languages where your test score is at least 60,
+# in descending order of the results.
 p my_languages({"Java" => 10, "Ruby" => 80, "Python" => 65})
 # prints ["Ruby", "Python"]
 p my_languages({"Hindi" => 60, "Dutch" => 93, "Greek" => 71})
