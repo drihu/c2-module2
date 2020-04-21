20c53a1232a2c551a1df7f1a5c88763db4fb2ad1
Ricardo Huamani <ricardohuamanip@gmail.com>
2020-04-21 09:55:40 -0500

    add create_csv.rb

---
+++ week-2/day-2/create_csv.rb
@@ -0,0 +1,38 @@
+# Struct is another built in class from the standard library.
+# It allows to define Classes that only have some properties on the fly
+Student = Struct.new(:name, :email)
+# Same as:
+# class Student
+#   attr_accessor :name, :email
+#   def initialize(name, email)
+#    @name = name
+#    @email = email
+#   end
+# end
+#
+
+# Generates a CSV string from the students.
+def generate_csv(students)
+  output = "name,email\n"
+  students.each do |student|
+    output << "#{student.name},#{student.email}\n"
+  end
+  output
+end
+
+students = [
+  Student.new("Julio", "jnavarro@able.co"),
+  Student.new("ximena", "ximena@able.co"),
+  Student.new("diego", "diego@able.co"),
+  Student.new("jordi", "jordi@able.co"),
+  Student.new("andres", "andres@able.co"),
+]
+
+puts generate_csv(students)
+# Outputs:
+# name,email
+# julio,jnavarro@able.co
+# ximena,ximena@able.co
+# diego,diego@able.co
+# jordi,jordi@able.co
+# andres,andres@able.co
