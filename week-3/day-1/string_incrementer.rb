89d60a59c28b6ef9c2816e8ff3819a65f7dd3971
Ricardo Huamani <ricardohuamanip@gmail.com>
2020-04-27 21:39:53 -0500

    use String#succ method

--- week-3/day-1/string_incrementer.rb
+++ week-3/day-1/string_incrementer.rb
@@ -1,14 +1,5 @@
 def increment_string(string)
-  case
-  when string.match?(/0+\z/)
-    string.gsub(/0\z/) { |match| '1' }
-  when string.match?(/0+9+\z/)
-    string.gsub(/09+\z/) { |match| (match.to_i + 1).to_s }
-  when string.match?(/[\d]+\z/)
-    string.gsub(/[1-9][0-9]*\z/) { |match| (match.to_i + 1).to_s }
-  else
-    string << '1'
-  end
+  string.gsub(/[\d]+\z/) { |match| (match.empty? ? '1' : match.succ) }
 end
 
 p increment_string("foo") # prints "foo1"
