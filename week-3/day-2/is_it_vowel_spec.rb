de3e260d5aca918f9e9b216f9e2a06e1b58d767f
Ricardo Huamani <ricardohuamanip@gmail.com>
2020-04-29 08:28:00 -0500

    add more specs

--- week-3/day-2/is_it_vowel_spec.rb
+++ week-3/day-2/is_it_vowel_spec.rb
@@ -6,12 +6,27 @@ end
 
 describe String do
   context '#vowel?' do
-    it "verifies if it's a vowel" do
+    it "returns false because it's not a vowel" do
       expect("".vowel?).to eq false
+    end
+
+    it "returns true because it's a vowel" do
       expect("a".vowel?).to eq true
+    end
+
+    it "returns true because it's a vowel" do
       expect("E".vowel?).to eq true
+    end
+
+    it "returns false because it's not a vowel" do
       expect("ou".vowel?).to eq false
+    end
+
+    it "returns false because it's not a vowel" do
       expect("z".vowel?).to eq false
+    end
+
+    it "returns false because it's not a vowel" do
       expect("lol".vowel?).to eq false
     end
   end
